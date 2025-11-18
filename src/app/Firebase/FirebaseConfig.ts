4dc71921afd291325805ec9165795224be70e07e
GabiiBarbosa <gabriellebarbosa597@gmail.com>
2025-11-18 03:07:50 -0300

    third commit

--- src/app/Firebase/FirebaseConfig.ts
+++ src/app/Firebase/FirebaseConfig.ts
@@ -1,3 +1,4 @@
+'use client';
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
